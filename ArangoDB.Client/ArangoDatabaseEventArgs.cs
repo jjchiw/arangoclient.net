133c5d4d84bb11e010f67855e4cf2143a16136d8
Juan J. Chiw <jjchiw@gmail.com>
2015-03-14 19:26:52 +0100

    add events to save, update, delete

---
+++ ArangoDB.Client/ArangoDatabaseEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArangoDB.Client
+{
+    public class ArangoDatabaseEventArgs : EventArgs
+    {
+        public object Item { get; set; }
+        public Type Type { get; set; }
+    }
+}
